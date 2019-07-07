380dfe48a5c3285ce3cd09e9ca3d6094e19937a2
Michal Gaborik <michal.gaborik.77@gmail.com>
2019-07-07 20:07:46 +0200

    example of how to implement rigidbody and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movment = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movment * speed);
+    }
+}
